aab1cc79ef6129dcaa429047634d71ab1c62d2f1
Nadiya Putri Intan Nur Rahmadhani <nadiyarahmadhani481@gmail.com>
2025-10-30 15:03:27 +0700

    Upload Tugas PBO Kesembilan

---
+++ PertemuanKesebelas/build/generated-sources/ap-source-output/TokoBunga/Tokobunga_1_.java
@@ -0,0 +1,19 @@
+package TokoBunga;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-10-30T13:27:50", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Tokobunga_1.class)
+public class Tokobunga_1_ { 
+
+    public static volatile SingularAttribute<Tokobunga_1, String> ukuran;
+    public static volatile SingularAttribute<Tokobunga_1, String> idbunga;
+    public static volatile SingularAttribute<Tokobunga_1, Integer> harga;
+    public static volatile SingularAttribute<Tokobunga_1, String> kategoribunga;
+    public static volatile SingularAttribute<Tokobunga_1, String> deskripsiproduk;
+    public static volatile SingularAttribute<Tokobunga_1, Integer> stok;
+    public static volatile SingularAttribute<Tokobunga_1, String> namabunga;
+
+}
